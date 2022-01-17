7d50437c9677f5d60250a9b19ff56fe0887f7609
nishikant <nishikat@gmail.com>
2022-01-17 13:11:27 +0530

    third commit

--- lightsail-containers-flask/app.py
+++ lightsail-containers-flask/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return "Wellcome neosoft "
+   return "Wellcome  "
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=5000)
